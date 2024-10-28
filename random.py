76cacbe1b491d6a1d727711ea585ef1aa3b39150
MahrozAbbas <dt.sci@outlook.com>
2024-10-28 14:57:47 +0500

    Create random.py

---
+++ random.py
@@ -0,0 +1,13 @@
+from random import *
+class randomgenerate:   
+    def __is_prime__(num):
+        if num==2:
+            return True
+        elif num%2==0:
+            return False
+        else:
+            ran=int(num**.5)+1
+            for i in range(3,ran,2):
+                if num%i==0:
+                    return False
+        return True  
